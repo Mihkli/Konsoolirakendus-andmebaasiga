d51a7449c8c7a01425f9177ee2519956eb6e644c
Mihkli <kevinmihkli@gmail.com>
2022-02-18 13:10:41 +0200

    first commit

---
+++ SQLiteCountries/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace SQLiteCountries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadCountries();
+        }
+
+        private static void ReadCountries()
+        {
+            Database databaseObj = new Database();
+            string query = "SELECT Country.courntyname as Country, Capital.capitalname as Capital from Capital JOIN Country on Capital.countryId = Country.rowid";
+            SQLiteCommand mycommand = new SQLiteCommand(query, databaseObj.myConnection);
+            databaseObj.OpenConnection();
+
+            SQLiteDataReader data = mycommand.ExecuteReader();
+
+            if (data.HasRows)
+            {
+                while (data.Read())
+                {
+                    Console.WriteLine($"Country: {data["Country"]}. Capital: {data["Capital"]}");
+                }
+            }
+
+            databaseObj.CloseConnection();
+        }
+
+    }
+}
